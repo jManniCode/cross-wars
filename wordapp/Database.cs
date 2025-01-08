05c6bb417d3ea8724cbc9131f41e771c59a82572
huineith <westholm.jacob@gmail.com>
2025-01-08 08:39:44 +0100

    Progress from first day of pair porgraming

--- wordapp/Database.cs
+++ wordapp/Database.cs
@@ -9,7 +9,7 @@ public class Database
     private readonly string _host = "localhost";
     private readonly string _port = "5432";
     private readonly string _username = "postgres";
-    private readonly string _password = "root"; // root OR postgres
+    private readonly string _password = "portedinme"; // root OR postgres
     private readonly string _database = "database_for_wordapp";
 
     private NpgsqlDataSource _connection;
