0c00aa3054335d6508565b746467eb80838ebaa1
okzScripts <oskar.krantz90@gmail.com>
2025-01-09 14:51:49 +0100

    tagit bort wordapp-mappen från Benji

--- wordapp/WordRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace Wordapp;
-
-// Model to parse the NewWord POST route request body (Note that a record would do just as well, this is just an example o how a regular class is typically defined for a post body)
-public class WordRequest
-{
-    public string Word { get; set; }
-}
