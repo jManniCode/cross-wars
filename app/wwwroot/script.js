f5a495380e540402cab5436f173a86ded4c301be
huineith <westholm.jacob@gmail.com>
2025-01-09 10:32:48 +0100

    progressed on script.js

--- app/wwwroot/script.js
+++ app/wwwroot/script.js
@@ -1,10 +1,27 @@
-const crossword = document.getElementById("grid-container");
+const crossword = document.getElementById("crossword");
 
 // Loop to create 100 grid items (10x10 grid)
-for (let i = 1; i <= 100; i++) {
+const gridlist=[]; 
+for(let j=0; j<10 ; j++){
+    gridlist.push([]); 
+}
+for (let i = 0; i < 100; i++) {
     // Create a new div element for each grid item
     const gridItem = document.createElement("div");
+    gridlist[ Math.floor(i/10)].push(gridItem); 
     gridItem.classList.add("grid-item");
+    
+    gridItem.classList.add("inactive")
     gridItem.textContent = i; // Add a number to each cell
-    gridContainer.appendChild(gridItem); // Add to the grid container
+    crossword.appendChild(gridItem); // Add to the grid container
 }
+
+
+const tile= document.getElementby
+
+
+
+
+
+
+
