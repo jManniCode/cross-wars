d88b7901c78d5bb752d5e5b58d930ef72dba1830
okzScripts <oskar.krantz90@gmail.com>
2025-01-09 13:23:15 +0100

    lagt till databaskoppling och testat

--- app/Database.cs
+++ app/Database.cs
@@ -1,6 +1,26 @@
 namespace app;
 
+using Npgsql;
 public class Database
 {
+    private readonly string _host = "localhost";
+    private readonly string _port = "5432";
+    private readonly string _username = "postgres";
+    private readonly string _password = "1234";
+    private readonly string _database = "crosswarsdatabase";
+
+    private NpgsqlDataSource _connection;
+
+    public NpgsqlDataSource Connection()
+    {
+        return _connection;
+    } 
     
+    public Database()
+    {
+        // bygg en anslutningssträng (Adress och inloggning till databasen) 
+        string connectionString = $"Host={_host};Port={_port};Username={_username};Password={_password};Database={_database}";
+        // använd den för att hämta en anslutning
+        _connection = NpgsqlDataSource.Create(connectionString);
+    }
 }
